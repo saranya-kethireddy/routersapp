44ab517cd4d4dea612928b396fa511880c67539b
saranya-kethireddy <saranyakethireddy@gmail.com>
2021-11-25 11:30:49 +0530

    Nested Routing

---
+++ src/pages/EditProfile.js
@@ -0,0 +1,5 @@
+const EditProfile = () => {
+    return <h2>Edit Profile Page</h2>;
+};
+
+export default EditProfile
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,18 +1,41 @@
 import { useEffect } from "react";
-import { useHistory } from "react-router";
+
+import { Route, Switch, useHistory, useRouteMatch } from "react-router";
+import { Link } from "react-router-dom";
+import EditProfile from "./EditProfile";
+import ViewProfile from "./ViewProfile";
 
 const Profile = ({ login }) => {
 
-   const history = useHistory();
+    const history = useHistory();
     useEffect(() => {
-        if(!login) {
+        if (!login) {
             history.push('/about');
-        }   
+        }
     }, [login, history]);
+    const { path, url } = useRouteMatch()
 
     return (
-        <h1>profile page</h1>
-    )
+        <>
+
+            <h1>PROFILE PAGE</h1>
+
+            <ul>
+                <li>
+                    <Link to={`${url}/viewprofile`}>View Profile</Link>
+                </li>
+                <li>
+                    <Link to={`${url}/editprofile`}>Edit Profile</Link>
+                </li>
+            </ul>
+
+            <Switch>
+                <Route path={`${path}/viewprofile`} component={ViewProfile} />
+                <Route path={`${path}/editprofile`} component={EditProfile} />
+
+            </Switch>
+        </>
+    );
 }
 
 export default Profile
