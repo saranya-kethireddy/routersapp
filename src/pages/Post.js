32cac44c3e856737fd2aeca49cffe7d6d03cf42c
saranya-kethireddy <saranyakethireddy@gmail.com>
2021-11-25 11:01:51 +0530

    use location and use params

--- src/pages/Post.js
+++ src/pages/Post.js
@@ -1,10 +1,19 @@
-import { useParams } from "react-router";
+import { useLocation, useParams } from "react-router";
 
 const Post = () => {
 
     const { id } = useParams();
 
-    return <h2>ID is = {id}</h2>
+    const query = new URLSearchParams(useLocation().search)
+
+
+    return (
+        <>
+            <h2>ID is = {id}</h2>
+            <h2>{query.get("first")}</h2>
+            <h2>{query.get("last")}</h2>
+        </>
+    )
 };
 
 export default Post
